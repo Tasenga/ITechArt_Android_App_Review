ff558419460adcea9d48bdfa1310403803f25eaf
Anastasia Orlovskaya <nastassia.orlovskaya@gmail.com>
2020-01-31 15:49:18 +0300

    Data source is added, and also library to get main dictionary from source is created.

---
+++ source/get_data.py
@@ -0,0 +1,15 @@
+import json
+import os
+import gzip
+
+def open_gzip():
+    archivename = 'reviews_Apps_for_Android_5.json.gz'
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    with gzip.open(os.path.join(ROOT_DIR, archivename), 'rb') as gzip_file:
+            my_dict = {}
+            i = 1
+            for row in gzip_file:
+                my_dict[i] = json.loads(row)
+                i += 1
+    return my_dict
