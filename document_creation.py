66b2902c38f9e913512cfc771e461a30ad476a80
Anastasia Orlovskaya <nastassia.orlovskaya@gmail.com>
2020-02-05 20:43:57 +0300

    class 'file_create' was splited up on 2 functions 'create_directory',  'save_file'.

--- document_creation.py
+++ document_creation.py
@@ -2,20 +2,19 @@ import os
 import csv
 
 
-class file_create:
-    def directory_create(self):
-        '''function creates a directory to save files'''
-        try:
-            os.mkdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resulting data'))
-        except:
-            pass
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resulting data')
-        return path
+def create_directory():
+    '''function creates a directory to save files'''
+    try:
+        os.mkdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resulting data'))
+    except:
+        pass
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resulting data')
+    return path
 
-    def save_file(self, name, data, mode='w'):
-        '''function creates a file from the transferred data'''
-        with open(os.path.join(self.directory_create(), name), mode, newline='') as csv_file:
-            writer = csv.writer(csv_file, delimiter='\t')
-            for line in data:
-                writer.writerow(line)
-            writer.writerow('')
+def save_file(name, data, mode='w'):
+    '''function creates a file from the transferred data'''
+    with open(os.path.join(create_directory(), name), mode, newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter='\t')
+        for line in data:
+            writer.writerow(line)
+        writer.writerow('')
