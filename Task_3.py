c6bcd8eaae67db3b03f9c104a21773ddf1f06cd0
Anastasia Orlovskaya <nastassia.orlovskaya@gmail.com>
2020-02-05 21:04:00 +0300

    Function 'message_list' was separated for 2 functions 'get_text' and 'split_text_up' to optimize this part of code:
    >>>   for item in main_dict.values():
    >>>        	if 'overall' in item.keys():
    >>>           		if item['overall'] >= 2.5:
    >>>                			positive_comments.extend(re.split(r'\W+', item['reviewText'].replace("'", '_').lower()))
    >>>           		else:
    >>>                 			negative_comments.extend(re.split(r'\W+', item['reviewText'].replace("'", '_').lower()))

--- Task_3.py
+++ Task_3.py
@@ -1,28 +1,30 @@
 from source import get_data
 import re
-from document_creation import file_create
+from document_creation import save_file
 from operator import itemgetter
 
-def message_list(main_dict):
-    '''function returns positive and negative messages list'''
-    positive_comments = []
-    negative_comments = []
-    for item in main_dict.values():
-        if 'overall' in item.keys():
-            if item['overall'] >= 2.5:
-                positive_comments.extend(re.split(r'\W+', item['reviewText'].replace("'", '_').lower()))
-            else:
-                negative_comments.extend(re.split(r'\W+', item['reviewText'].replace("'", '_').lower()))
-    return [positive_comments, negative_comments]
 
-def popular_word_list(message_list):
+def get_text(main_dict, overall_values):
+    '''function gets a text and returns list of reviews in depends on overall values'''
+    message_list = [item['reviewText'] for item in main_dict.values()
+                    if 'overall' in item.keys() and item['overall'] in overall_values]
+    return message_list
+
+def split_text_up(text):
+    '''function gets a text and returns list of words from this text'''
+    list_of_words = []
+    for review in text:
+        list_of_words.extend(re.split(r'\W+', review.replace("'", '_').lower()))
+    return list_of_words
+
+def popular_word_list(words_list):
     '''function returns the most popular words from word_list
 
     3.1. Task_3.py: to create file words-stats1.cvs и words-stats2.cvs
     containing information about the most popular words from positive and negative messages.
     '''
     popular_in_positive_comment = {}
-    for word in message_list:
+    for word in words_list:
         if word != '':
             count = popular_in_positive_comment.get(word, 0)
             popular_in_positive_comment[word] = count + 1
@@ -31,8 +33,9 @@ def popular_word_list(message_list):
 
 if __name__ == "__main__":
     main_dict = get_data.open_gzip()
-    positive_comments, negative_comments = message_list(main_dict)
-    word_in_positive = popular_word_list(positive_comments)
-    file_create().save_file('words-stats1.cvs', sorted(word_in_positive.items(), key=itemgetter(1), reverse=True))
-    word_in_negative = popular_word_list(negative_comments)
-    file_create().save_file('words-stats2.cvs', sorted(word_in_negative.items(), key=itemgetter(1), reverse=True))
+    words_in_positive_comments = popular_word_list(split_text_up(get_text(main_dict, range(4, 6))))
+    save_file('words-stats1.cvs',
+                            sorted(words_in_positive_comments.items(), key=itemgetter(1), reverse=True))
+    words_in_negative_comments = popular_word_list(split_text_up(get_text(main_dict, range(1, 4))))
+    save_file('words-stats2.cvs',
+                            sorted(words_in_negative_comments.items(), key=itemgetter(1), reverse=True))
