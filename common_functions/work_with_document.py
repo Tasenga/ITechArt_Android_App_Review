5c86f4d188a3b4ccd9d8ac4ff125ffc9fed5f82f
Anastasia Orlovskaya <nastassia.orlovskaya@gmail.com>
2020-02-09 21:01:14 +0300

    function save_result was optimized with using Path.mkdir,
    added incoming parameter path, which requested from user

--- common_functions/work_with_document.py
+++ common_functions/work_with_document.py
@@ -1,20 +1,14 @@
-import os
+from os.path import dirname, abspath, split, join
 import csv
+from pathlib import Path
 
 
-def create_directory():
-    '''function creates a directory to save files'''
-    try:
-        os.mkdir(os.path.join(os.environ['PYTHONPATH'], 'resulting data'))
-    except:
-        pass
-    path = os.path.join(os.environ['PYTHONPATH'], 'resulting data')
-    return path
-
-def save_file(name, data, mode='w'):
+def save_file(path, name, data, mode='w'):
     '''function creates a file from the transferred data'''
-    with open(os.path.join(create_directory(), name), mode, newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=':')
+
+    Path(join(path, 'resulting data')).mkdir(parents=True, exist_ok=True)
+    with open(join(path, 'resulting data', name), mode, newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter='\t')
         for line in data:
             writer.writerow(line)
         writer.writerow('')
