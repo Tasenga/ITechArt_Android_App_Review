72ded4d9e1c0434fd96528afe1fad3ad764db570
Anastasia Orlovskaya <nastassia.orlovskaya@gmail.com>
2020-02-10 13:21:52 +0300

    function 'save_file' was rewritten more idiomatic

--- common_functions/work_with_document.py
+++ common_functions/work_with_document.py
@@ -6,9 +6,10 @@ from pathlib import Path
 def save_file(path, name, data, mode='w'):
     '''function creates a file from the transferred data'''
 
-    Path(join(path, 'resulting data')).mkdir(parents=True, exist_ok=True)
-    with open(join(path, 'resulting data', name), mode, newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter='\t')
+    out_dir = Path(join(path, "resulting data"))
+    out_dir.mkdir(parents=True, exist_ok=True)
+    with Path(out_dir, name).open(mode=mode, newline="") as csv_file:
+        writer = csv.writer(csv_file, delimiter="\t")
         for line in data:
             writer.writerow(line)
-        writer.writerow('')
+        writer.writerow("")
